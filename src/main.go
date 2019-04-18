610ad00c0c2d97ae44f9c566af8c4906c5d0a153
Joko Irianto <irianto94@outlook.com>
2019-04-18 16:59:10 +0700

    command router

---
+++ src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		err = CommandRouter(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func CommandRouter(commandStr string) error {
+
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+
+	switch arrCommandStr[0] {
+	case "create_parking_lot":
+
+	case "park":
+
+	case "leave":
+
+	case "status":
+
+	case "slot_numbers_for_cars_with_colour":
+
+	case "slot_number_for_registration_number":
+
+	case "help":
+
+	case "exit":
+		os.Exit(0)
+	}
+
+	return nil
+}
